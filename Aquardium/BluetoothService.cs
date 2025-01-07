ef22fdaa7cf9a5eb8d406f339495e0d81ebcd149
rowel <rowel.babol14@gmail.com>
2025-01-07 17:39:33 +0800

    set subscriber method to automatically retrieve service instead of hard-coding the UUID.

--- Aquardium/BluetoothService.cs
+++ Aquardium/BluetoothService.cs
@@ -9,151 +9,150 @@ using Plugin.BLE.Abstractions.Contracts;
 using Plugin.BLE.Abstractions.EventArgs;
 using Plugin.BLE.Abstractions.Exceptions;
 
-namespace Aquardium
+namespace Aquardium;
+
+public class BluetoothService
 {
-    public class BluetoothService
+    private readonly IAdapter _adapter;
+    private readonly Label statusLabel;
+    private readonly Button reconnectButton;
+
+    public BluetoothService(Label statusLabel, Button reconnectButton)
     {
-        private readonly IAdapter _adapter;
-        private readonly Label statusLabel;
-        private readonly Button reconnectButton;
+        this.statusLabel = statusLabel;
+        this.reconnectButton = reconnectButton;
+        this.statusLabel.Text = "Connecting via Bluetooth...";
+        _adapter = CrossBluetoothLE.Current.Adapter;
+        _adapter.DeviceConnected += HandleArduinoConnected;
+        _adapter.DeviceConnectionLost += HandleArduinoDisconnected;
 
-        public BluetoothService(Label statusLabel, Button reconnectButton)
+        Connect();
+    }
+
+    public void Connect()
+    {
+        var knownDevices = _adapter.GetSystemConnectedOrPairedDevices();
+        foreach (var device in knownDevices)
         {
-            this.statusLabel = statusLabel;
-            this.reconnectButton = reconnectButton;
-            this.statusLabel.Text = "Connecting via Bluetooth...";
-            _adapter = CrossBluetoothLE.Current.Adapter;
-            _adapter.DeviceConnected += HandleArduinoConnected;
-            _adapter.DeviceConnectionLost += HandleArduinoDisconnected;
-
-            Connect();
-        }
-
-        public void Connect()
-        {
-            var knownDevices = _adapter.GetSystemConnectedOrPairedDevices();
-            foreach (var device in knownDevices)
+            if (device.Name.Contains("arduino"))
             {
-                if (device.Name.Contains("arduino"))
+                MainThread.BeginInvokeOnMainThread(async () =>
                 {
-                    MainThread.BeginInvokeOnMainThread(async () =>
+                    try
                     {
-                        try
-                        {
-                            await _adapter.ConnectToDeviceAsync(device);
-                            statusLabel.Text = $"Connected to {device.Name}! Please wait...";
-                        }
-                        catch (DeviceConnectionException dce)
-                        {
-                            statusLabel.Text = $"Failed to connect: {dce.Message}";
-                            reconnectButton.IsEnabled = true;
-                        }
-                    });
-                }
-            }
-        }
-
-        private async void HandleArduinoConnected(object? sender, DeviceEventArgs e)
-        {
-            await SubscribeToDataAsync(e.Device);
-
-            MainThread.BeginInvokeOnMainThread (() =>
-            {
-                if (Application.Current.MainPage is MainPage mainPage)
-                {
-                    if (mainPage.Devices == null)
-                        mainPage.Devices = new System.Collections.ObjectModel.ObservableCollection<ArduinoDevice>();
-
-                    var device = mainPage.Devices.FirstOrDefault(d => d.Id == e.Device.Name);
-
-                    if (device == null) // If Arduino is not in the list, add it
-                    {
-                        device = new ArduinoDevice { Id = e.Device.Name, Status = "Connected" };
-                        mainPage.Devices.Add(device);
-                        mainPage.Detail = new NavigationPage(new ArduinoTabbedPage(device));
+                        await _adapter.ConnectToDeviceAsync(device);
+                        statusLabel.Text = $"Connected to {device.Name}! Please wait...";
                     }
-                    else // If Arduino is in the list, update its status
+                    catch (DeviceConnectionException dce)
                     {
-                        device.Status = "Connected";
-                        WeakReferenceMessenger.Default.Send(new StatusUpdateMessage(e.Device.Name, "Connected"));
+                        statusLabel.Text = $"Failed to connect: {dce.Message}";
+                        reconnectButton.IsEnabled = true;
                     }
-                }
-
-                else if (Application.Current.MainPage is NavigationPage navPage &&
-                 navPage.CurrentPage is ConnectionPage)
-                {
-                    var device = new ArduinoDevice
-                    {
-                        Id = e.Device.Name,
-                        Status = "Connected"
-                    };
-
-                    var newMainPage = new MainPage
-                    {
-                        Detail = new NavigationPage(new ArduinoTabbedPage(device))
-                    };
-
-                    newMainPage.Devices.Add(device);
-
-                    Application.Current.MainPage = newMainPage;
-                }
-            });
-        }
-
-        private void HandleArduinoDisconnected(object? sender, DeviceEventArgs e)
-        {
-            MainThread.BeginInvokeOnMainThread(() => {
-                if (Application.Current.MainPage is MainPage mainPage)
-                {
-                    var device = mainPage.Devices.FirstOrDefault(d => d.Id == e.Device.Name);
-
-                    if (device != null)
-                    {
-                        device.Status = "Disconnected";
-                        WeakReferenceMessenger.Default.Send(new StatusUpdateMessage(device.Id, "Disconnected"));
-                    }
-
-                    if (mainPage.Devices.All(d => d.Status == "Disconnected"))
-                        ShowReconnectionAlert();
-                }
-            });
-        }
-
-        private async void ShowReconnectionAlert()
-        {
-            bool reconnected = false;
-
-            // Loop until an Arduino reconnects
-            while (!reconnected)
-            {
-
-                await MainThread.InvokeOnMainThreadAsync(async () =>
-                {
-                    Connect();
-                    await (Application.Current.MainPage).DisplayAlert(
-                        "Connection Lost",
-                        "No Arduinos connected. Reconnecting...",
-                        "OK"
-                    );
                 });
-
-                if (Application.Current.MainPage is MainPage mainPage && mainPage.Devices.Any(s => s.Status == "Connected"))
-                {
-                    reconnected = true;
-                    break;
-                }
             }
         }
+    }
 
-        private async Task SubscribeToDataAsync(IDevice device)
+    private async void HandleArduinoConnected(object? sender, DeviceEventArgs e)
+    {
+        await SubscribeToDataAsync(e.Device);
+
+        MainThread.BeginInvokeOnMainThread (() =>
         {
-            var service = await device.GetServiceAsync(Guid.Parse("00001809-0000-1000-8000-00805f9b34fb"));  // Replace with Arduino Service UUID
-            var tempCharacteristic = await service.GetCharacteristicAsync(Guid.Parse("00002A6E-0000-1000-8000-00805f9b34fb"));  // Replace with Arduino Characteristic UUID
+            if (Application.Current.MainPage is MainPage mainPage)
+            {
+                if (mainPage.Devices == null)
+                    mainPage.Devices = new System.Collections.ObjectModel.ObservableCollection<ArduinoDevice>();
+
+                var device = mainPage.Devices.FirstOrDefault(d => d.Id == e.Device.Name);
+
+                if (device == null) // If Arduino is not in the list, add it
+                {
+                    device = new ArduinoDevice { Id = e.Device.Name, Status = "Connected" };
+                    mainPage.Devices.Add(device);
+                    mainPage.Detail = new NavigationPage(new ArduinoTabbedPage(device));
+                }
+                else // If Arduino is in the list, update its status
+                {
+                    device.Status = "Connected";
+                    WeakReferenceMessenger.Default.Send(new StatusUpdateMessage(e.Device.Name, "Connected"));
+                }
+            }
+
+            else if (Application.Current.MainPage is NavigationPage navPage &&
+                navPage.CurrentPage is ConnectionPage)
+            {
+                var device = new ArduinoDevice
+                {
+                    Id = e.Device.Name,
+                    Status = "Connected"
+                };
+
+                var newMainPage = new MainPage
+                {
+                    Detail = new NavigationPage(new ArduinoTabbedPage(device))
+                };
+
+                newMainPage.Devices.Add(device);
+
+                Application.Current.MainPage = newMainPage;
+            }
+        });
+    }
+
+    private void HandleArduinoDisconnected(object? sender, DeviceEventArgs e)
+    {
+        MainThread.BeginInvokeOnMainThread(() => {
+            if (Application.Current.MainPage is MainPage mainPage)
+            {
+                var device = mainPage.Devices.FirstOrDefault(d => d.Id == e.Device.Name);
+
+                if (device != null)
+                {
+                    device.Status = "Disconnected";
+                    WeakReferenceMessenger.Default.Send(new StatusUpdateMessage(device.Id, "Disconnected"));
+                }
+
+                if (mainPage.Devices.All(d => d.Status == "Disconnected"))
+                    ShowReconnectionAlert();
+            }
+        });
+    }
+
+    private async void ShowReconnectionAlert()
+    {
+        bool reconnected = false;
+
+        // Loop until an Arduino reconnects
+        while (!reconnected)
+        {
+            await MainThread.InvokeOnMainThreadAsync(async () =>
+            {
+                Connect();
+                await (Application.Current.MainPage).DisplayAlert(
+                    "Connection Lost",
+                    "No Arduinos connected. Reconnecting...",
+                    "OK"
+                );
+            });
+
+            if (Application.Current.MainPage is MainPage mainPage && mainPage.Devices.Any(s => s.Status == "Connected"))
+                reconnected = true;
+        }
+    }
+
+    private async Task SubscribeToDataAsync(IDevice device)
+    {
+        var services = await device.GetServicesAsync();
+
+        foreach (var service in services)
+        {
+            var tempCharacteristic = await service.GetCharacteristicAsync(Guid.Parse("00002a6e-0000-1000-8000-00805f9b34fb"));  // Temperature Characteristic UUID
 
             if (tempCharacteristic == null)
             {
                 Console.WriteLine("Failed to find characteristic.");
-                return;
+                continue;
             }
 
             tempCharacteristic.ValueUpdated += (o, args) =>
